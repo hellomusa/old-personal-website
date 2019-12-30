dbc89f7c520f7225004af67e5659a8179bd20b96
helloMusa <esquitoban@gmail.com>
2019-12-29 22:39:42 -0500

    added terminal box

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+@app.route("/projects")
+def projects():
+    return render_template('home.html')
+
+@app.route("/experiences")
+def experiences():
+    return render_template('home.html')
+
+@app.route("/writing")
+def writing():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
