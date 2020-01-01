13cd82ac374fc0e941c0a5ba62efc6e1e3cf1994
helloMusa <esquitoban@gmail.com>
2020-01-01 02:10:53 -0500

    Added admin login page

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from fetcher import *
+from forms import LoginForm
 
 app = Flask(__name__)
+
+# Get secret key from .txt file
+with open('tokens.txt', 'r') as f:
+	f.readline()
+	SECRET_KEY = f.readline().strip()
+
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route("/")
@@ -14,13 +22,20 @@ def home():
 def projects():
     return render_template('home.html')
 
-@app.route("/experiences")
-def experiences():
-    return render_template('home.html')
 
-@app.route("/writing")
-def writing():
-    return render_template('home.html')
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		# Placeholder login info
+		if form.username.data == 'musaali' and form.password.data == 'admin':
+			return redirect(url_for('home'))
+	return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
