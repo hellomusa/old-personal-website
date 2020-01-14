b2a6367edba9e25642fcc06013ad35260e3818a2
helloMusa <esquitoban@gmail.com>
2020-01-14 16:55:47 -0500

    Reordered routes

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template, url_for, redirect, request, Markup
-from app import app, db, bcrypt
-from markdown import markdown
-from app.forms import LoginForm, PostForm
-from app.fetcher import *
-from app.models import User, Post, Fetch
 from flask_login import login_user, current_user, login_required
+from markdown import markdown
+from app import app, db, bcrypt
+from app.fetcher import *
+from app.forms import LoginForm, PostForm
+from app.models import User, Post, Fetch
 
 
 @app.route("/")
@@ -19,31 +19,17 @@ def home():
 							comment_time=comment_time)
 
 
-@app.route("/projects")
-def projects():
-    return render_template('home.html')
-
-
 @app.route("/blog")
 def blog():
 	posts = Post.query.all()
 	return render_template('blog.html', posts=posts)
 
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('home'))
+@app.route('/blog/<post_title>')
+def post(post_title):
+	post = get_post(post_title)
 
-	form = LoginForm()
-
-	if form.validate_on_submit():
-		user = User.query.first()
-		if user and bcrypt.check_password_hash(user.password, form.password.data):
-			login_user(user, remember=False)
-			return redirect(url_for('home'))
-
-	return render_template('login.html', form=form)
+	return render_template('post.html',post=post)
 
 
 @app.route("/create", methods=['GET', 'POST'])
@@ -61,37 +47,6 @@ def create():
 	return render_template('create.html', form=form, legend='Create Post')
 
 
-@app.route('/blog/<post_title>')
-def post(post_title):
-	if not current_user.is_authenticated:
-		return redirect(url_for('login'))
-
-	post = get_post(post_title)
-
-	return render_template('post.html',post=post)
-
-
-@app.route('/blog/<post_title>/update', methods=['GET', 'POST'])
-def update(post_title):
-	if not current_user.is_authenticated:
-		return redirect(url_for('login'))
-
-	post = get_post(post_title)
-	form = PostForm()
-
-	if form.validate_on_submit():
-		post.title = form.title.data
-		post.subtitle = form.subtitle.data
-		post.content = form.content.data
-		db.session.commit()
-	elif request.method == 'GET':
-		form.title.data = post.title
-		form.subtitle.data = post.subtitle
-		form.content.data = post.content
-
-	return render_template('create.html', form=form, legend='Update Post')
-
-
 @app.route('/blog/<post_title>/delete', methods=['GET', 'POST'])
 def delete(post_title):
 	if not current_user.is_authenticated:
@@ -102,6 +57,22 @@ def delete(post_title):
 	db.session.commit()
 
 	return redirect(url_for('home'))
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+
+	form = LoginForm()
+
+	if form.validate_on_submit():
+		user = User.query.first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=False)
+			return redirect(url_for('home'))
+
+	return render_template('login.html', form=form)
 
 
 @app.template_filter("markdown")
