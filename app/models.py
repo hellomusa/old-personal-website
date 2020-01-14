063cac045341cb6efa52e4d54c5431f15f4d237c
helloMusa <esquitoban@gmail.com>
2020-01-14 16:55:34 -0500

    Reordered imports

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from app import db
-from app import db, login_manager
 from flask_login import UserMixin
+from app import db, login_manager
+
 
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
