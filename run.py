2252b181e4b88eb41c8bf8bc424b0bccbc22931e
helloMusa <esquitoban@gmail.com>
2020-01-17 20:24:08 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app
+from waitress import serve
 
 if __name__ == "__main__":
-    app.run(use_reloader=False, debug=True)
+    # app.run(use_reloader=False, debug=True)
+    serve(app, host='127.0.0.1', port=5000)
