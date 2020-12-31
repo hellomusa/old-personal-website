b8e313066bbe4eaa0dfe922540090961521e33ba
helloMusa <esquitoban@gmail.com>
2020-12-31 18:23:30 -0500

    Test github API

--- app/fetcher.py
+++ app/fetcher.py
@@ -114,41 +114,41 @@ def github_fetcher():
 
 	return ['TEST', 'TEST']
 
-	try:
-		response = requests.get(url, params=params)
-		data = json.loads(response.content)
+	# try:
+	# 	response = requests.get(url, params=params)
+	# 	data = json.loads(response.content)
 
-		for repo in data:
-			repo_names.append(repo['full_name'][10:])
+	# 	for repo in data:
+	# 		repo_names.append(repo['full_name'][10:])
 
-		for repo_name in repo_names:
-			commit_url = f'https://api.github.com/repos/hellomusa/{repo_name}/commits/master'
-			try:
-				response = requests.get(commit_url, params=params)
-				data = json.loads(response.content)
-				commit_date = data['commit']['author']['date']
-				commit_date_dt = datetime.strptime(commit_date, "%Y-%m-%dT%H:%M:%SZ")
-				commits[repo_name] = commit_date_dt
-			except requests.exceptions.RequestException as e:
-				return ['ERROR', 'ERROR']
+	# 	for repo_name in repo_names:
+	# 		commit_url = f'https://api.github.com/repos/hellomusa/{repo_name}/commits/master'
+	# 		try:
+	# 			response = requests.get(commit_url, params=params)
+	# 			data = json.loads(response.content)
+	# 			commit_date = data['commit']['author']['date']
+	# 			commit_date_dt = datetime.strptime(commit_date, "%Y-%m-%dT%H:%M:%SZ")
+	# 			commits[repo_name] = commit_date_dt
+	# 		except requests.exceptions.RequestException as e:
+	# 			return ['ERROR', 'ERROR']
 
-		commit_times = [commits[repo] for repo in commits]
-		newest_commit_time = max(commit_times)
-		newest_commit_repo = ''
+	# 	commit_times = [commits[repo] for repo in commits]
+	# 	newest_commit_time = max(commit_times)
+	# 	newest_commit_repo = ''
 
-		for repo in commits:
-			if commits[repo] == newest_commit_time:
-				newest_commit_repo = repo
+	# 	for repo in commits:
+	# 		if commits[repo] == newest_commit_time:
+	# 			newest_commit_repo = repo
 
-		current_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-		current_time_dt = datetime.strptime(current_time, "%Y-%m-%d %H:%M:%S")
-		time = get_time_difference(current_time_dt, newest_commit_time)
-		return_list = [newest_commit_repo, time]
+	# 	current_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+	# 	current_time_dt = datetime.strptime(current_time, "%Y-%m-%d %H:%M:%S")
+	# 	time = get_time_difference(current_time_dt, newest_commit_time)
+	# 	return_list = [newest_commit_repo, time]
 
-		return return_list
+	# 	return return_list
 
-	except requests.exceptions.RequestException as e:
-		return ['ERROR', 'ERROR']
+	# except requests.exceptions.RequestException as e:
+	# 	return ['ERROR', 'ERROR']
 
 
 
